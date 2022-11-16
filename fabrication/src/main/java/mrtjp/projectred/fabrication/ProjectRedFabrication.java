92092d2227db84ce2d1094264819b913d298b2b0
MrTJP <mrtjp@icloud.com>
2022-11-16 10:47:34 -0500

    feat: add fabrication recipes

--- fabrication/src/main/java/mrtjp/projectred/fabrication/ProjectRedFabrication.java
+++ fabrication/src/main/java/mrtjp/projectred/fabrication/ProjectRedFabrication.java
@@ -5,6 +5,7 @@ import codechicken.lib.util.SneakyUtils;
 import codechicken.multipart.api.MultiPartType;
 import mrtjp.projectred.fabrication.data.FabricationBlockStateModelProvider;
 import mrtjp.projectred.fabrication.data.FabricationItemModelProvider;
+import mrtjp.projectred.fabrication.data.FabricationRecipeProvider;
 import mrtjp.projectred.fabrication.init.*;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
@@ -75,6 +76,9 @@ public class ProjectRedFabrication {
         if (event.includeClient()) {
             generator.addProvider(new FabricationBlockStateModelProvider(generator, fileHelper));
             generator.addProvider(new FabricationItemModelProvider(generator, fileHelper));
+        }
+        if (event.includeServer()) {
+            generator.addProvider(new FabricationRecipeProvider(generator));
         }
     }
 }
