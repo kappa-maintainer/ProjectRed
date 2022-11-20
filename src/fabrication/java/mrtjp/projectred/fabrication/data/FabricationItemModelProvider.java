a0d57a192ca6273686d4cf71618078e06880905f
MrTJP <mrtjp@icloud.com>
2022-11-19 19:49:36 -0500

    fix: fabrication block breaking patricles

--- src/fabrication/java/mrtjp/projectred/fabrication/data/FabricationItemModelProvider.java
+++ src/fabrication/java/mrtjp/projectred/fabrication/data/FabricationItemModelProvider.java
@@ -23,13 +23,11 @@ public class FabricationItemModelProvider extends ItemModelProvider {
     @Override
     protected void registerModels() {
 
-        getSimple(IC_WORKBENCH_BLOCK)
-                .parent(new ModelFile.UncheckedModelFile(modLoc("block/" + name(IC_WORKBENCH_BLOCK) + "_blueprint")))
-                .texture(null);
+        simpleItemBlock(IC_WORKBENCH_BLOCK);
 
-        domedMachine(PLOTTING_TABLE_BLOCK);
-        domedMachine(LITHOGRAPHY_TABLE_BLOCK);
-        domedMachine(PACKAGING_TABLE_BLOCK);
+        simpleItemBlock(PLOTTING_TABLE_BLOCK);
+        simpleItemBlock(LITHOGRAPHY_TABLE_BLOCK);
+        simpleItemBlock(PACKAGING_TABLE_BLOCK);
 
         generated(IC_BLUEPRINT_ITEM);
         generated(BLANK_PHOTOMASK_ITEM);
@@ -40,11 +38,5 @@ public class FabricationItemModelProvider extends ItemModelProvider {
         generated(INVALID_DIE_ITEM);
 
         generated(FABRICATED_GATE_ITEM).texture(null); // Dummy model to suppress warnings (actually rendered runtime via IItemRenderer)
-   }
-
-   private void domedMachine(Block block) {
-       getSimple(block)
-               .parent(new ModelFile.UncheckedModelFile(modLoc("block/" + name(block) + "_c0")))
-               .texture(null);
    }
 }
