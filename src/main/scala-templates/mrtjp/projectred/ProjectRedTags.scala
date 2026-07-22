050a8b0164ab999653dc958a96fa3aa9510a53e0
arcade_kappa <gkappa@outlands.top>
2026-07-22 14:57:14 +0800

    Basic setup

---
+++ src/main/scala-templates/mrtjp/projectred/ProjectRedTags.scala
@@ -0,0 +1,13 @@
+package {{ package }}
+
+object ProjectRedTags {
+  final val MOD_ID = "{{ mod_id }}"
+  final val MOD_NAME = "{{ mod_name }}"
+  final val VERSION = "{{ mod_version }}"
+  final val SCALA_VERSION = "{{ scala_version }}"
+  final val MINECRAFT_VERSION = "{{ minecraft_version }}"
+  final val TJPCORE_VERSION = "{{ tjpcore_version }}"
+  final val CCL_VERSION = "{{ ccl_version }}"
+  final val FMP_VERSION = "{{ fmp_version }}"
+  final val JEI_VERSION = "{{ jei_version }}"
+}
